30537f7c0a9f687929eb507e62cd08baa2543686
Michael Staatz <michael@staatzstreich.de>
2024-09-10 07:56:42 +0200

    Added first route and dummy data

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<h1>
+    The list of tasks
+</h1>
+
+@if (count($tasks))
+    @forelse ($tasks as $task)
+        <div>
+            <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+        </div>
+    @empty
+        <div>There are no tasks!</div>
+    @endforelse
+@endif
