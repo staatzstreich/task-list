cb31ba648979b6c259f2fadc3c46e90e2c1a72d2
Michael Staatz <michael@staatzstreich.de>
2024-09-10 12:45:40 +0200

    Added some sort of error handling with the session

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,33 @@
 <?php
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn () => redirect()->route('tasks.index'));
 
 Route::get('/tasks', function () {
-    return view('index', ['tasks' => \App\Models\Task::latest()->get()]);
+    return view('index', ['tasks' => Task::latest()->get()]);
 })->name('tasks.index');
 
 Route::view('/tasks/create', 'create')->name('tasks.create');
 
 Route::get('/{id}', function ($id) {
-    return view('show', ['task' => \App\Models\Task::findOrFail($id)]);
+    return view('show', ['task' => Task::findOrFail($id)]);
 })->name('tasks.show');
 
 Route::post('/tasks', function(Request $request) {
-    dd($request->all());
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required'
+    ]);
+
+    $task = new Task;
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+    $task->save();
+
+    return redirect()->route('tasks.show', ['id' => $task->id])->with('success', 'Task created successfully!');;
 })->name('tasks.store');
