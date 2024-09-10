0cd2d3c6eca2138f7b30435b05e744ff70a7e454
Michael Staatz <michael@staatzstreich.de>
2024-09-10 12:24:17 +0200

    Implementing a form for new tasks and update routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn () => redirect()->route('tasks.index'));
@@ -8,6 +9,12 @@ Route::get('/tasks', function () {
     return view('index', ['tasks' => \App\Models\Task::latest()->get()]);
 })->name('tasks.index');
 
+Route::view('/tasks/create', 'create')->name('tasks.create');
+
 Route::get('/{id}', function ($id) {
     return view('show', ['task' => \App\Models\Task::findOrFail($id)]);
 })->name('tasks.show');
+
+Route::post('/tasks', function(Request $request) {
+    dd($request->all());
+})->name('tasks.store');
