842371ab6db922506743fcb61517a7cf62602cb1
Michael Staatz <michael@staatzstreich.de>
2024-09-10 10:19:32 +0200

    Added an Layout and modified the templates

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,15 @@
-<h1>
-    The list of tasks
-</h1>
+@extends('layouts.app')
 
-@if (count($tasks))
-    @forelse ($tasks as $task)
-        <div>
-            <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
-        </div>
-    @empty
-        <div>There are no tasks!</div>
-    @endforelse
-@endif
+@section('title', 'The list of tasks')
+
+@section('content')
+    @if (count($tasks))
+        @forelse ($tasks as $task)
+            <div>
+                <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+            </div>
+        @empty
+            <div>There are no tasks!</div>
+        @endforelse
+    @endif
+@endsection
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,8 +1,12 @@
-<h1>{{ $task->title }}</h1>
-<p>{{ $task->description }}</p>
-@if ($task->long_description)
-<p>{{ $task->long_description }}</p>
-@endif
+@extends('layouts.app')
 
-<p>{{ $task->created_at }}</p>
-<p>{{ $task->updated_at }}</p>
+@section('title', $task->title)
+
+@section('content')
+    <p>{{ $task->description }}</p>
+    @if ($task->long_description)
+        <p>{{ $task->long_description }}</p>
+    @endif
+    <p>{{ $task->created_at }}</p>
+    <p>{{ $task->updated_at }}</p>
+@endsection
