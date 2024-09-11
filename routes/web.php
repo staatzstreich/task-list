d2034b542f701addc67aade72c1dcc1e81098b2e
Michael Staatz <michael@staatzstreich.de>
2024-09-11 05:37:38 +0200

    Delete an Task

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,9 @@ Route::put('/tasks/{task}', function (Task $task, TaskRequest $request) {
 
     return redirect()->route('tasks.show', ['task' => $task->id])->with('success', 'Task saved successfully!');
 })->name('tasks.update');
+
+Route::delete('/tasks/{task}', function(Task $task) {
+    $task->delete();
+
+    return redirect()->route('tasks.index')->with('success', 'Task deleted successfully!');
+})->name('tasks.destroy');
